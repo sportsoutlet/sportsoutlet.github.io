2300a62feec338fef88a472324f3035a0a7a7ca8
Plant <caleb.j.alvarado3431@gmail.com>
2025-05-20 02:13:02 -0400

    frontend 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       {userInfo.name ?
         sport ? settingTeam ? <TeamSelect sport={sport} setSettingTeam={setSettingTeam} setTeams={setTeams} teams={teams} setSport={setSport} /> :
           activeTeam ? <GameSummaryPage activeTeam={activeTeam} name={userInfo.name} setActiveTeam={setActiveTeam} /> :
-            <MySports teams={teams} setSettingTeam={setSettingTeam} setSport={setSport} setActiveTeam={setActiveTeam} /> :
+            <MySports teams={teams} setSettingTeam={setSettingTeam} setSport={setSport} setActiveTeam={setActiveTeam} setTeams={setTeams} /> :
           <SportSelect setSport={setSport} /> :
         <RegisterForm setInfo={setUserInfo} />
 
--- client/src/MySports.jsx
+++ client/src/MySports.jsx
@@ -1,28 +1,41 @@
 import './my-sports.css'
-import { Plus } from 'lucide-react';
+import { Plus, X } from 'lucide-react';
 
 
-function MySports({teams, setSettingTeam, setSport, setActiveTeam}){
+function MySports({ teams, setSettingTeam, setSport, setActiveTeam, setTeams }) {
 
-    function handleClick(){
+    function handleClick() {
         setSettingTeam(true);
         setSport();
     }
 
- return(
-    <div className='my-sports-wrapper'>
-        <h1 className='pt-5'>Your Teams:</h1>
-        <div className='my-sports py-3'>
-            <button className='flex items-center justify-center' onClick={handleClick}><Plus/></button>
-            {teams.map((team) => (
-                <button key={team.team} onClick={() => setActiveTeam(team.team)}>
-                    <span className='main text-lg font-bold'>{team.team}</span>
-                    <span className='sub text-sm mt-1 font-thick'>{team.sport}</span>
+    function handleRemove(index) {
+        setTeams(prev => prev.filter((_, i) => i !== index));
+    }
+
+    return (
+        <div className='my-sports-wrapper'>
+            <h1 className='pt-5'>Your Teams:</h1>
+            <div className='my-sports py-3'>
+                <button className='flex items-center justify-center' onClick={handleClick}><Plus /></button>
+                {teams.map((team, index) => (
+                    <button key={team.team} onClick={() => setActiveTeam(team.team)}>
+                        <span className='main text-lg font-bold pb-2'>{team.team}</span>
+                        <span className='sub text-sm absolute bottom-3 left-1/2 -translate-x-1/2 font-thick'>{team.sport}</span>
+                        <button
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handleRemove(index);
+                            }}
+                            aria-label="Remove team"
+                        >
+                            <X size={18} />
+                        </button>
                     </button>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>
- )
+    )
 }
 
 export default MySports;
